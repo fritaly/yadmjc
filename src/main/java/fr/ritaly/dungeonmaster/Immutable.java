d9d1c56890f4ea280bce219f267e703de0343e55
Francois Ritaly <francois.ritaly@gmail.com>
2010-11-03 23:20:56 +0100

    Created annotation Immutable.java

---
+++ src/main/java/fr/ritaly/dungeonmaster/Immutable.java
@@ -0,0 +1,10 @@
+package fr.ritaly.dungeonmaster;
+
+/**
+ * Annotation used to tag an object whose state is immutable.
+ * 
+ * @author <a href="mailto:francois.ritaly@free.fr">Francois RITALY</a>
+ */
+public @interface Immutable {
+
+}
